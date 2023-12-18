7b923d130c1ef63893f81ffcc9c8a3cd7c03ecfb
IgorGryp <igor.gryp@gmail.com>
2023-12-18 13:58:53 +0100

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import db from './database.js';
 // --------- Header/Nav/Meny ------------
 
 // Menyknapp - hamburgermeny
--- src/js/todaysDishes.js
+++ src/js/todaysDishes.js
@@ -1,4 +1,4 @@
-import db from './db.js';
+import db from './database.js';
 
 console.log(db);
 console.log(db['best-foods'][0]);
