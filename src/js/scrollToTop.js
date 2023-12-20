4b2da76489183aba3d5cfcba08ebaab241f9cb2e
Kim Andersson <kandersson@gmail.com>
2023-12-20 20:38:50 +0100

    scroll to top fix

--- src/js/scrollToTop.js
+++ src/js/scrollToTop.js
@@ -1,8 +1,23 @@
+// function scrollToTop(elementId) {
+//   var element = document.getElementById(elementId);
+//   console.log(element);
+
+//   if (element) {
+//     element.scrollIntoView({ behavior: 'smooth' });
+//   }
+// }
+
 function scrollToTop(elementId) {
-  var element = document.getElementById(elementId);
+  const element = document.getElementById(elementId);
   console.log(element);
 
   if (element) {
-    element.scrollIntoView({ behavior: 'smooth' });
+    // Lägg till eller dra bort 90px beroende på behov
+    const offset = 90;
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({
+      top: elementPosition - offset,
+      behavior: 'smooth'
+    });
   }
 }
